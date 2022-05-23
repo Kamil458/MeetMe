f1538af1f37338b9b710210bf6ab06e5b09112b1
Jasiu2028@gmail.com <2028Jasiu>
2022-05-24 00:50:25 +0200

    Add AddView

--- src/main/java/com/MeetMe/ui/AddView.java
+++ src/main/java/com/MeetMe/ui/AddView.java
@@ -1,8 +1,57 @@
 package com.MeetMe.ui;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datetimepicker.DateTimePicker;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import java.time.Duration;
+
+@PageTitle("MeetMe create event")
 @Route("add")
-public class AddView extends Composite {
+public class AddView extends VerticalLayout {
+
+
+    public AddView() {
+
+        TextArea title = new TextArea("Title");
+        title.setWidth("50%");
+
+        TextArea localization = new TextArea("Localization");
+        localization.setWidth("50%");
+
+        TextArea description = new TextArea("Description");
+        description.setWidth("50%");
+
+        DateTimePicker dateTimePicker = new DateTimePicker();
+        dateTimePicker.setLabel("Meeting date and time");
+        dateTimePicker.setStep(Duration.ofMinutes(15));
+        dateTimePicker.setWidth("50%");
+
+        Button createButton = new Button("Create");
+        createButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        VerticalLayout layout = new VerticalLayout(
+                new H1("Create your own event!"),
+                title,
+                dateTimePicker,
+                localization,
+                description,
+                createButton
+        );
+
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        add(layout);
+
+    }
 }
